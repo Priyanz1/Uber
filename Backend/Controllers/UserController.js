d9a661f2f517aa0a97e0d8dc09adc0b5252d66a9
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2025-09-30 23:05:07 +0530

    loginauth all done

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -4,28 +4,32 @@ const UserModel = require("../Models/UserModel");
 
 
 const Login= async (req,res)=>{
-    const {email,password}= req.body;
-    const user=await UserModel.findOne({email:email}).select("+password");
-    if(!user){
-      return res.json({msg:"Invalid Email or Password"});
-    }
+try{
+  const {email,password}= req.body;
+  const user=await UserModel.findOne({email:email}).select("+password");
+  if(!user){
+    return res.json({msg:"Invalid Email or Password"});
+  }
 
-    const isMatch= await bcrypt.compare(password,user.password);
-    if(!isMatch){
-       return res.json({msg:"Invalid Email or Password"});
-    }
-   const token= jwt.sign({email:email},"secret");
+  const isMatch= await bcrypt.compare(password,user.password);
+  if(!isMatch){
+     return res.json({msg:"Invalid Email or Password"});
+  }
+ const token= jwt.sign({email:email},"secret");
 
-   res.cookie("token", token, {
-    httpOnly: true,
-    secure: true, 
-    sameSite: "strict",
-    maxAge: 60 * 60 * 1000
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true, 
+  sameSite: "strict",
+  maxAge: 60 * 60 * 1000
+});
 
-  res.json({ message: "Logged in successfully" });
-    // jwt.verify({password:user.password},"secret");
-    
+res.json({ message: "Logged in successfully" });
+
+}catch (err) {
+  console.error(err);
+  res.status(500).json({ msg: "Server error" });
+}
 
 
 }
--- Backend/middleware/UserLoginAuth.js
+++ Backend/middleware/UserLoginAuth.js
@@ -1,10 +1,21 @@
 const UserModel=require("../Models/UserModel");
 const LoginAuth= async (req,res,next)=>{
-  const token=req.cookies.token;
-  if(!token){
-    res.json({msg:"login first"});
-  }
-  const user=await UserModel.findOne({})
+ try{
+    const token=req.cookies.token;
+    if(!token){
+      res.json({msg:"login first"});
+    }
+    const decoded= jwt.verify(token,"secret");
+    const user=await UserModel.findOne({email:decoded.email});
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    req.user=user;
+    next();
+ }catch(err){
+    console.error(err);
+    res.status(401).json({ msg: "Token is not valid" });
+ }
 
 }
 module.exports = LoginAuth;
