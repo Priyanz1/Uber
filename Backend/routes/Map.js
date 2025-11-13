3c25d488bb526b93b0f6a88a13367648f3e94377
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2025-11-13 19:43:30 +0530

    remove unwanted code from map

--- Backend/routes/Map.js
+++ Backend/routes/Map.js
@@ -4,12 +4,6 @@ const { getCoordinates, getDistanceAndTime, calculateFare, getAutoSuggestions } 
 const LoginAuth = require("../middleware/UserLoginAuth");
 
 
-// router.post("/getcoordinates",CaptainLoginAuth, getCoordinates);
-// router.post("/distancetime",CaptainLoginAuth, getDistanceAndTime);
-// router.post("/calculatefare",CaptainLoginAuth, calculateFare);
-// router.post("/get-suggestions",CaptainLoginAuth, gtAutoSuggestions);
-// module.exports = router;
-
 router.get("/getcoordinates",LoginAuth,getCoordinates);
 router.get("/distancetime", LoginAuth,getDistanceAndTime);
 router.get("/getsuggestions",LoginAuth,getAutoSuggestions);
